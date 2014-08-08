4ddd5b7a4527476ac0353fed2327cdd2ab7ad3f0
tristantao <tao.tristan@gmail.com>
2014-08-08 12:12:41 -0700

    grabs all the links, only 1 region

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import requests
 from datetime import date
 
 def get_raw_html(raw_link):
+    #returns the raw html of a page, errors and exits if request doesn't go through
     print "[STATUS] Scraping {0}".format(raw_link)
     r = requests.get(raw_link)
-    if(r.status_code != requests.codes.ok):
+    if (r.status_code != requests.codes.ok):
         print "WARN: Request did not come back with OK status code for: %s \nExiting" % raw_link
         exit(1)
     raw_html = r.text
@@ -28,9 +29,9 @@ def get_listing_url(search_link):
 
 
 
-def generate_search_url(region, term, min_price, max_price):
-    static_base = "http://%s.craigslist.org/search/sss?query=%s&minAsk=%s&maxAsk=%s&sort=rel"
-    return static_base % (region, term, min_price, max_price)
+def generate_search_url(region, page, term, min_price, max_price):
+    static_base = "http://%s.craigslist.org/search/sss?$s=%s&query=%s&minAsk=%s&maxAsk=%s&sort=rel"
+    return static_base % (region, page, term, min_price, max_price)
 
 
 if __name__ == '__main__':
@@ -39,8 +40,18 @@ if __name__ == '__main__':
     SEARCH_TERM = 'motorcycle'
     MIN_PRICE = 0
     MAX_PRICE = 0
+    OUTPUT_PREFIX = 'moto_'
+    CURRENT_PAGE = 0
 
-    print search_term
     while True:
-        listing_batch = get_listing_ur
-        pass
+        search_url = generate_search_url(REGIONS[0], CURRENT_PAGE, SEARCH_TERM, MIN_PRICE, MAX_PRICE)
+        ad_links = get_listing_url(search_url)
+        #crawl_and_output(ad_links, OUTPUT_PREFIX)
+
+        if len(ad_links) == 0:
+            CURRENT_PAGE = 0
+            break
+        else:
+            CURRENT_PAGE += 100
+
+
